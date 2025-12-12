8c61bbcb35a3013483dd2f0a8c1a68c32c9aec42
devsoftwareapp <devsoftwaremail@gmail.com>
2025-12-12 19:53:22 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,16 +5,17 @@ plugins {
 
 android {
     namespace = "com.devsoftware.pdfreader"
-    compileSdk = 34
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.devsoftware.pdfreader"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
         vectorDrawables {
             useSupportLibrary = true
         }
@@ -29,36 +30,34 @@ android {
             )
         }
     }
-    
+
+    // Modern Java/Kotlin ayarları
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
-    
+
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
-    
+
     buildFeatures {
         viewBinding = true
     }
 }
 
 dependencies {
-    // Core
+    // Core Android
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
-    
-    // Material 3
-    implementation("androidx.compose.material3:material3:1.1.2")
-    
-    // WebView
+
+    // Modern WebView Component
     implementation("androidx.webkit:webkit:1.9.0")
-    
-    // Testing
+
+    // Test
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)

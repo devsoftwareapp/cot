eeccdec43a88179f5e5cc92c1b357f378db4351c
devsoftwareapp <devsoftwaremail@gmail.com>
2025-12-12 19:58:29 +0300

    Create MainActivity.kt

---
+++ app/src/main/com/devsoftware/pdfreader/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.devsoftware.pdfreader
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.devsoftware.pdfreader.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.textViewHello.text = "Hello Android SDK 36 🎉"
+    }
+}
